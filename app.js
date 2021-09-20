f2c4b3967440a7b12a398aef18e455b56d2a2ae7
yuliadrozdova <drozdova.exceedteam@gmail.com>
2021-09-20 10:34:06 +0300

    Create application

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+//const bodyParser = require('body-parser');
+//const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+
+const apiRoutes = require("./src/modules/routes/routes");
+
+//////////app.use(cors());
+
+const uri = "mongodb+srv://user_01:CK9qTqZ5@cluster0.ijpew.mongodb.net/TestAppDB?retryWrites=true&w=majority"
+mongoose.connect(uri);
+
+//////////app.use(bodyParser.json());
+app.use("/", apiRoutes);
+
+app.listen(8080, () => {
+    console.log('Example app listening on port 8080!')
+})
+
+app.listen(8080, () => {
+    console.log('Example app listening on port 8080!');
+    console.error;
+})
